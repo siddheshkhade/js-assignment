aaf44a46570f348ada35d14df74501780393270d
siddheshkhade <68239633+siddheshkhade@users.noreply.github.com>
2020-07-16 23:32:43 +0530

    Add files via upload

---
+++ f1.js
@@ -0,0 +1,12 @@
+//Question 1:
+// Write a JS code which takes input from the user and logs it in the console.
+
+
+console.log('Solution 1');
+//The Modal plugin is a dialog bob or popup window that is displayed on the top of the current page.
+
+
+//prompt
+//syntax:prompt("title","default value"[optional])
+let name=prompt('Tell me your name','write your name here');
+console.log(name);
